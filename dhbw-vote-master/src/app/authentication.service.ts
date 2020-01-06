f0e0b824e26ae5e2ee8f39d20dfbf89211102a78
fortender <timgubener@web.de>
2020-01-06 23:54:04 +0100

    Added authentication service

---
+++ dhbw-vote-master/src/app/authentication.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { User } from './user';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('user')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  login(username: string, password: string) {
+    return this.http.post<any>(`${environment.apiUrl}/users/authenticate`, { username, password })
+      .pipe(map(user => {
+        // Successful login -> store jwt token in local storage
+        if (user && user.token) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSubject.next(user);
+        }
+        return user;
+      }));
+  }
+
+  logout() {
+    // Clean local storage from user data
+    localStorage.removeItem('user');
+    this.currentUserSubject.next(null);
+  }
+
+}
